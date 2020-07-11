0f3ca99162d8beebfbf3152ba3d38b16d2752037
danielhorowitz <daniel.horowitz@n26.com>
2020-07-11 22:35:24 +0200

    Read github app credentials from env vars

--- app/src/main/java/com/dhorowitz/keepr/MainActivity.kt
+++ app/src/main/java/com/dhorowitz/keepr/MainActivity.kt
@@ -2,11 +2,15 @@ package com.dhorowitz.keepr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dhorowitz.keepr.BuildConfig.KEEPR_CLIENT_ID
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "client id = $KEEPR_CLIENT_ID", Toast.LENGTH_LONG).show()
     }
 }
