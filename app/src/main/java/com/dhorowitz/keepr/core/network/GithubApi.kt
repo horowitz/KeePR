e7e18ce1bd01d2ff56c1dfcaa54644bc4ce670ba
danielhorowitz <daniel.horowitz@n26.com>
2020-07-11 21:51:37 +0200

    Remove workerthread annotation from GithubApi

--- app/src/main/java/com/dhorowitz/keepr/core/network/GithubApi.kt
+++ app/src/main/java/com/dhorowitz/keepr/core/network/GithubApi.kt
@@ -1,13 +1,11 @@
 package com.dhorowitz.keepr.core.network
 
-import androidx.annotation.WorkerThread
 import com.dhorowitz.keepr.core.network.model.PRDto
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GithubApi {
-    @WorkerThread
     @GET("/repos/{org}/{repo}/pulls")
     suspend fun getPRs(@Path("org") org: String,
                        @Path("repo") repo: String): Response<List<PRDto>>
